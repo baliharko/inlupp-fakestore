4a70227e076439ce0c756d5d02a92b12eb1a0474
baliharko <bali@fauxdelorean.com>
2021-03-26 11:57:30 +0100

    localstorage and cart working

--- app.js
+++ app.js
@@ -1,49 +1,19 @@
 $(document).ready(() => {
 
     const endPoint = 'https://webacademy.se/fakestore/';
-
     const aboutModal = document.createElement('div');
+
     let allProducts = [];
-    let cart = [];
-
-    aboutModal.innerHTML = 
-    `
-    <div id="myModal" class="modal fade" tabindex="-1" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content text-center">
-          <div class="modal-header align-self-center">
-            <h5 class="modal-title m-2">About</h5>
-          </div>
-          <div class="modal-body">
-            <p class="lead">You won't find a store more fake than this...</p>
-            <p class="mx-5 mt-2 mb-4">
-              A fake store fetching data from an API and implementing basic
-              ecommerce functionality.
-            </p>
-            <button class="btn btn-outline-secondary" id="exitModal">
-              Cool!
-            </button>
-          </div>
-          <div class="modal-footer d-flex justify-content-start">
-            <p class="small my-0" style="font-size: xx-small">
-              baliharko - 2021
-            </p>
-          </div>
-        </div>
-      </div>
-    </div
-    `;
-
-    document.body.insertAdjacentElement('beforeend', aboutModal);
 
     load();
 
+    let cart = (localStorage.getItem('cart') === null) ? [] : JSON.parse(localStorage.getItem('cart'));
+    
     function load() {
         fetch(endPoint)
           .then(response => response.json())
           .then(json => {
-            allProducts = json;
-            console.log(allProducts);
+            allProducts = json;            
             render(json)
           })
           .catch(err => console.error(err))
@@ -78,27 +48,72 @@ $(document).ready(() => {
         $('#productView').html(output);
 
         $('.addToCartBtn').on('click', function() {                  
-
-          const selected = allProducts.find(item => item.id == this.id);
-          cart.push(selected);
           
-          let amount = getProductAmount(selected);
+          const selected = allProducts.find(item => item.id == this.id);
+          cart.push(selected);          
+          const amount = getProductAmount(selected);
+
+          localStorage.setItem('cart', JSON.stringify(cart));             
           console.log(amount);
           console.log(cart);
-        })
+      })
     }
 
-    $('#modalButton').click(() => {
-      $('#myModal').modal('show');
-    });
 
-    $('#exitModal').click(() => {
-      $('#myModal').modal('hide');
-    });
 
     function getProductAmount(product) {
       return cart.reduce((acc, val) => (
         val.id === product.id ? acc + 1 : acc 
       ), 0); 
     }
+
+    $('#clearLsBtn').on('click', () => {
+        clearLocalstorage();        
+    })
+
+    // Debug - rensar localstorage och laddar om sidan
+    function clearLocalstorage() {
+        localStorage.clear();
+        window.location.reload(true);
+        console.log('cleared localstorage & reloaded page');
+    }
+
+        aboutModal.innerHTML = 
+    `
+    <div id="myModal" class="modal fade" tabindex="-1" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content text-center">
+          <div class="modal-header align-self-center">
+            <h5 class="modal-title m-2">About</h5>
+          </div>
+          <div class="modal-body">
+            <p class="lead">You won't find a store more fake than this...</p>
+            <p class="mx-5 mt-2 mb-4">
+              A fake store fetching data from an API and implementing basic
+              ecommerce functionality.
+            </p>
+            <button class="btn btn-outline-secondary" id="exitModal">
+              Cool!
+            </button>
+          </div>
+          <div class="modal-footer d-flex justify-content-start">
+            <p class="small my-0" style="font-size: xx-small">
+              baliharko - 2021
+            </p>
+          </div>
+        </div>
+      </div>
+    </div
+    `;
+
+    document.body.insertAdjacentElement('beforeend', aboutModal);
+
+    $('#modalButton').click(() => {
+        $('#myModal').modal('show');
+    });
+
+    $('#exitModal').click(() => {
+        $('#myModal').modal('hide');
+    });
 });
+
